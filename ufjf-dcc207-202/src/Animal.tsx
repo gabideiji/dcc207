9359d4078534ecb54f8091082c195624df00b069
Gabriella Margarida Ribeiro Soares <gabriellamargarida18@gmail.com>
2025-10-07 22:03:34 -0300

    adiciona props

--- ufjf-dcc207-202/src/Animal.tsx
+++ ufjf-dcc207-202/src/Animal.tsx
@@ -1,5 +1,17 @@
 import "./Animal.css"
-
-export default function Animal (){
-    return (<div className="animal">Animal</div>);
+type AnimalProps = {
+    icone?: string;
+    nome?: string;
+    peso?: number;
+    emExtincao?: boolean;
+}
+export default function Animal ({icone="?", nome="Desconhecido", peso=0.1, emExtincao=false}:
+    AnimalProps){
+    return (
+    <div className="animal">
+        <div>{icone}</div>
+        <div>{nome}</div>
+        <div>{peso.toLocaleString()}kg</div>
+        <div>{emExtincao}</div>
+    </div>);
 }
